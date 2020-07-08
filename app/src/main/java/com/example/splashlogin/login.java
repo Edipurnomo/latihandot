c0109689feef7f45b5be37c2669cf6cb5fca8b8f
Edipurnomo <edipurnomo71196@gmail.com>
2020-07-08 16:54:04 +0700

    tes

--- app/src/main/java/com/example/splashlogin/login.java
+++ app/src/main/java/com/example/splashlogin/login.java
@@ -2,14 +2,17 @@ package com.example.splashlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class login extends AppCompatActivity {
     private Button buttonLogin;
+    private TextView Register;
 
 
     @Override
@@ -18,6 +21,7 @@ public class login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         buttonLogin = findViewById(R.id.buttonLogin);
+        Register = findViewById(R.id.Register);
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +31,16 @@ public class login extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v){
+                // action
+                Intent intent = new Intent(login.this, NewReg.class);
+                startActivity(intent);
+            }
+        });
+
+
 
     }
 }
